8192480ec97f43217eb3bf8aa4b1f980fa242a3a
Sullyvan Puppi <spuppi@gmail.com>
2018-06-04 16:56:27 -0300

    Controle de fluxo.

---
+++ HelloWorld_GitEclipse/src/br/com/spuppi/view/ControleFluxo2.java
@@ -0,0 +1,22 @@
+package br.com.spuppi.view;
+
+public class ControleFluxo2 {
+
+	public static void main(String[] args) {
+		byte dia = 1;
+		for (;dia<=7;dia++) {
+			if(dia==1 || dia == 7 ) {
+				System.out.println("Dia " + dia + ", é final de semana!");
+			}else {
+				System.out.println("Dia " + dia + ", é dia útil!");
+			}
+			switch(dia) {
+			case 1: case 7:
+				System.out.println("Dia " + dia + ", é final de semana!");
+			case 2: case 3: case 4: case 5: case 6:
+				System.out.println("Dia " + dia + ", é dia útil!");
+			}
+		}
+	}
+
+}
