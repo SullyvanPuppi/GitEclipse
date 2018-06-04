5b154b85e36f5f8060a21193e25f250f4f04542f
Sullyvan Puppi <spuppi@gmail.com>
2018-06-04 17:05:22 -0300

    Adicionado detalhes na exibição do resultado.

--- HelloWorld_GitEclipse/src/br/com/spuppi/view/ControleFluxo2.java
+++ HelloWorld_GitEclipse/src/br/com/spuppi/view/ControleFluxo2.java
@@ -6,15 +6,15 @@ public class ControleFluxo2 {
 		byte dia = 1;
 		for (;dia<=7;dia++) {
 			if(dia==1 || dia == 7 ) {
-				System.out.println("Dia " + dia + ", é final de semana!");
+				System.out.println("[If]Dia " + dia + ", é final de semana!");
 			}else {
-				System.out.println("Dia " + dia + ", é dia útil!");
+				System.out.println("[If]Dia " + dia + ", é dia útil!");
 			}
 			switch(dia) {
 			case 1: case 7:
-				System.out.println("Dia " + dia + ", é final de semana!");
+				System.out.println("[Switch]Dia " + dia + ", é final de semana!");
 			case 2: case 3: case 4: case 5: case 6:
-				System.out.println("Dia " + dia + ", é dia útil!");
+				System.out.println("[Switch]Dia " + dia + ", é dia útil!");
 			}
 		}
 	}
