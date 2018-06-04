c92109f87b19007530bbc9dc42dd97523bda9679
Sullyvan Puppi <spuppi@gmail.com>
2018-06-04 15:58:26 -0300

    Edição no Windows!

--- HelloWorld_GitEclipse/src/br/com/spuppi/view/ExemploGit.java
+++ HelloWorld_GitEclipse/src/br/com/spuppi/view/ExemploGit.java
@@ -5,6 +5,7 @@ public class ExemploGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World, Git Eclipse!");
 		//Editado no git
+		System.out.println("Editado no WIndows!");
 	}
 
 }
