52d7f2cd29b3dc684ebda2d782d096838d1d2b7a
Sullyvan Puppi <spuppi@gmail.com>
2018-06-04 17:15:57 -0300

    Controle de Fluxo 3.

---
+++ HelloWorld_GitEclipse/src/br/com/spuppi/view/ControleFluxo3.java
@@ -0,0 +1,14 @@
+package br.com.spuppi.view;
+
+class ControleFluxo3 {
+
+	public static void main(String[] args) {
+		//Imprimir todos os número pares de 0 até 100000
+		for(int i = 0;i<=100000;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
